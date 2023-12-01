dd0a8c89a37a6756de9333deae9f686a07870c28
peshr17 <132731207+peshr17@users.noreply.github.com>
2023-12-01 22:04:56 +0700

    Add files via upload

---
+++ installer1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "จะลง gui ต้องใช้ความจำ 1.73Gจะลงหรือไม่"
+read -p "จะลงก็กด y ไม่ลงก็กด n นะ: " user_input
+
+if [ "$user_input" == "y" ]; then
+clear
+    echo "รอจนกว่าจะเสร็จ"
+termux-setup-storage && apt upgrade -y && apt install x11-repo && apt install termux-x11-nightly -y && apt install xfce4 xfce4-goodies xfce4-session -y && echo 'termux-x11 :1 -xstartup "dbus-launch --exit-with-session xfce4-session"' >> $HOME/startgui.sh
+    echo "ลงเสร็จแล้ว"
+else
+    echo "โอดค"
+fi
