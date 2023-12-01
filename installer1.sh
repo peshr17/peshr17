91dd20032595e2f8f1102f244e1e782e582a4cb7
peshr17 <132731207+peshr17@users.noreply.github.com>
2023-12-01 22:15:59 +0700

    installer1.sh

--- installer1.sh
+++ installer1.sh
@@ -6,7 +6,7 @@ read -p "จะลงก็กด y ไม่ลงก็กด n นะ: " user
 if [ "$user_input" == "y" ]; then
 clear
     echo "รอจนกว่าจะเสร็จ"
-termux-setup-storage && apt upgrade -y && apt install x11-repo && apt install termux-x11-nightly -y && apt install xfce4 xfce4-goodies xfce4-session -y && echo 'termux-x11 :1 -xstartup "dbus-launch --exit-with-session xfce4-session"' >> $HOME/startgui.sh && chmod 777 startgui.sh
+termux-setup-storage && apt upgrade -y && apt install x11-repo && apt install termux-x11-nightly -y && apt install xfce4 xfce4-goodies xfce4-session -y && echo 'termux-x11 :1 -xstartup "dbus-launch --exit-with-session xfce4-session"' >> startgui.sh && chmod 777 startgui.sh
     echo "ลงเสร็จแล้วแล้วก็พิมพ์ ./startgui.sh"
 else
     echo "โอดค"
